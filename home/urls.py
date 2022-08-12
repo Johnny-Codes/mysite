7244596051dbe1fdf14eb1aed54a324665f099c8
paul <johnspaulm@gmail.com>
2022-08-12 13:14:32 -0500

    css playground page added

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index, name="home"),
+    path('cssplayground/', views.css_playground, name="cssplayground"),
 ]
